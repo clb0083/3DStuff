d274112ba26da7ed3771a41f2da3994f785852f8
Graham Duke <164713847+ghd0009@users.noreply.github.com>
2024-07-29 17:34:44 -0500

    The Actual Final Team 1 Update
    Fixed script header typos.

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -1,8 +1,9 @@
-/*AU Team 1 (SP & SU 2024 used the help of Auburn graduate student Jake Botello
-to learn Unity and C# in integratinf design ideas. The team applied background
+/*AU Team 1 (SP & SU 2024) used the help of Auburn graduate student Jake Botello
+to learn Unity and C# in integrating design ideas. The team applied background
 knowledge of MATLAB and C++ coding languages to develop various tools and
 functions used throughout this script. ChatGPT was also used as a troubleshooting
 reference.*/
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
