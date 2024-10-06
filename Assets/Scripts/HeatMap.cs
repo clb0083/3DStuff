75be91855d7bb5f83b720a9f019af5ac28710abd
CarsonReams <czr0072@auburn.edu>
2024-10-06 16:42:12 -0500

    Minor Bug Fix
    -Accidentally changed "ConductorTrigger" to "Trigger" in an earlier update. Reverted that change. Heat map works again

--- Assets/Scripts/HeatMap.cs
+++ Assets/Scripts/HeatMap.cs
@@ -45,7 +45,7 @@ public class HeatMap : MonoBehaviour
     //once heatmap is on, this will run constantly, and updates the colors as bridges form.
     public void heatMapToggleON()
     {
-        GameObject[] triggerObjects = GameObject.FindGameObjectsWithTag("Trigger");
+        GameObject[] triggerObjects = GameObject.FindGameObjectsWithTag("ConductorTrigger");
 
         foreach (GameObject triggerObject in triggerObjects)
         {
