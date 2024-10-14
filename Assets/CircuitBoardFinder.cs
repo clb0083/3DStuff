da0e240176ed93421e002b1254de1446f6fbb573
NathanNichols-sd <nwn0003@auburn.edu>
2024-10-14 16:32:39 -0500

    Revert "Spin added"
    This reverts commit 76b24685fcdd67dad2870abe7f101163eb189f75.

--- Assets/CircuitBoardFinder.cs
+++ Assets/CircuitBoardFinder.cs
@@ -3,15 +3,10 @@ using TMPro;
 
 public class CircuitBoardFinder : MonoBehaviour
 {
-    // References to the TMP input fields for rotation
+    // References to the TMP input fields
     public TMP_InputField CircuitRotateX;
     public TMP_InputField CircuitRotateY;
     public TMP_InputField CircuitRotateZ;
-
-    // References to the TMP input fields for spin rates
-    public TMP_InputField SpinRateX;
-    public TMP_InputField SpinRateY;
-    public TMP_InputField SpinRateZ;
 
     // This method will be called when the button is clicked
     public void OnButtonClick()
@@ -29,14 +24,6 @@ public class CircuitBoardFinder : MonoBehaviour
             // Set the rotation of the circuitBoard
             circuitBoard.transform.rotation = Quaternion.Euler(xRotation, yRotation, zRotation);
             Debug.Log($"CircuitBoard rotated to: X={xRotation}, Y={yRotation}, Z={zRotation}");
-
-            // Get the spin rates, defaulting to 0 if input is empty
-            float xSpinRate = GetInputValue(SpinRateX);
-            float ySpinRate = GetInputValue(SpinRateY);
-            float zSpinRate = GetInputValue(SpinRateZ);
-
-            // Start spinning the circuitBoard (you can implement a method to handle this)
-            StartCoroutine(SpinCircuitBoard(circuitBoard, xSpinRate, ySpinRate, zSpinRate));
         }
         else
         {
@@ -58,17 +45,7 @@ public class CircuitBoardFinder : MonoBehaviour
         {
             return result;
         }
-
+        
         return 0f; // Default to 0 if input is invalid
-    }
-
-    // Coroutine to handle the spinning
-    private System.Collections.IEnumerator SpinCircuitBoard(GameObject circuitBoard, float xSpinRate, float ySpinRate, float zSpinRate)
-    {
-        while (true)
-        {
-            circuitBoard.transform.Rotate(xSpinRate * Time.deltaTime, ySpinRate * Time.deltaTime, zSpinRate * Time.deltaTime);
-            yield return null; // Wait for the next frame
-        }
     }
 }
